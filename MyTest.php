c21f24c85e10958f1c77a49f7f658ca81df71d05
cclehui <god_chen@sina.com>
2017-08-17 20:18:40 +0800

    init

---
+++ MyTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace cclehui;
+
+class MyTest {
+    public static function test() {
+        echo "package cclehui , class MyTest , function test\n";
+    }
+}
---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+namespace cclehui;
+
+require_once(__DIR__ . "/MyTest.php");
+
+echo "---- this is cclehui/config.php\n";
